13bc8cf461b215a1d078d0b73138180a19d2c336
fabiodcs <your@email.com>
2022-08-03 12:05:37 -0300

    working

--- EventProject/Program.cs
+++ EventProject/Program.cs
@@ -1,8 +1,17 @@
+using EventProject.Controllers;
+using MediatR;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IMediatrHandler, MediatrHandler>();
+builder.Services.AddScoped<IInventoryService, InventoryService>();
+builder.Services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
