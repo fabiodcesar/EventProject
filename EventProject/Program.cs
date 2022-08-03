0ca1fa20a94762ea243b070a65b7b6e4465bb606
fabiodcs <your@email.com>
2022-08-03 14:44:11 -0300

    Cleaning up

--- EventProject/Program.cs
+++ EventProject/Program.cs
@@ -7,10 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-builder.Services.AddScoped<IMediatrHandler, MediatrHandler>();
 builder.Services.AddScoped<IInventoryService, InventoryService>();
-builder.Services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
